540e31f7c73bfced12249e03818df28a6d31f417
Patrick Fernandes <patrick.fernandes@city.ac.uk>
2020-07-03 22:17:42 +0100

    deleted older versions

--- GenerateTrafficLights.py
+++ GenerateTrafficLights.py
@@ -28,117 +28,34 @@ class GenerateTraffic:
     def runProgram(self):
         # threading.Timer(1.0, self.runProgram).start()
         while(True):
-            index = 0
-            light = self.traffic_whole
-            current_time = datetime.now().strftime("%H:%M:%S:%f")
-            while index < len(light):
-                # time.sleep(1)
-                #initial variable
-                if light[index].position == self.counter:
-                    time.sleep(1)
-                    # print("counter: " + str(self.counter))
-                    # print("number:" + str(self.number))
-                    # print("index: " + str(index))
+            if self.number == 1:
+                print("rip")
+            else:
+                index = 0
+                light = self.traffic_whole
+                current_time = datetime.now().strftime("%H:%M:%S")
+                while index < len(light):
+                    if light[index].position == self.counter:
+                        time.sleep(1)
+                        light[index].state = "on"
 
-                    light[index].state = "on"
-                    print(current_time + " " + str(light[index]))
-                #turn of behind initial variable from 0-len(9)
-                elif light[index].position == self.counter - 1:# and (self.counter-1) >= 0 and self.counter < self.number:
-                    # print("counter: " + str(self.counter))
-                    # print("number:" + str(self.number))
-                    # print("index: " + str(index))
+                        print(current_time + " " + str(light[index]))
 
-                    light[index].state = "off"
-                    print(current_time + " " + str(light[index]))
-                #reach end of loop
-                elif self.counter == self.number:
-                    # print("counter: " + str(self.counter))
-                    # print("number:" + str(self.number))
-                    # print("index: " + str(index))
+                    #turn of behind initial variable from 0-len(9)
+                    elif light[index].position == self.counter - 1:# and (self.counter-1) >= 0 and self.counter < self.number:
+                        light[index].state = "off"
 
-                    light[self.counter - 1].state = "off"
-                    self.counter = 0
-                    index = -1
-                    print(current_time + " " + str(light[index]))
-                index += 1
+                        print(current_time + " " + str(light[index]))
 
-            # print("---")
-            self.counter += 1
-            # time.sleep(1)
+                    #reach end of loop
+                    elif self.counter == self.number:
+                        light[self.counter - 1].state = "off"
+                        self.counter = 0
+                        index = -1
 
-    # def runProgram(self):
-    #
-    #     while(True):
-    #         i = 0
-    #         for light in self.traffic_whole:
-    #             #if position == [1,2,3], turn on
-    #             if light.position == self.counter:
-    #                 light.state = "on"
-    #                 print(light)
-    #             elif light.position == self.counter - 1 and (self.counter-1) >= 0 and self.counter < self.number:
-    #                 light.state = "off"
-    #                 print(light)
-    #             elif self.counter == self.number:
-    #                 self.counter = 0
-    #                 print(light)
-    #             else:
-    #                 #pass
-    #                 print(light)
-    #
-    #         self.counter += 1
-    #         print("---")
-    #
-    #
-    #         time.sleep(1)
+                        print(current_time + " " + str(light[index]))
+                    else:
+                        pass
+                    index += 1
+                self.counter += 1
 
-
-
-
-
-
-
-    # def runProgram(self):
-    #     threading.Timer(1.0, self.runProgram).start()
-    #     current_time = datetime.now().strftime("%H:%M:%S:%f")
-    #     for light in self.traffic_whole:
-    #
-    #         if self.number == 1:
-    #             self.isOneTrafficLight(light)
-    #         else:
-    #             # print states that are on
-    #             if light.position == self.counter:
-    #                 light.state = "on"
-    #                 print(current_time + " " + str(light) + " counter: " + str(self.counter) +" " +str(self.number))
-    #             # print states behind counter
-    #             elif light.position == self.counter - 1:
-    #                 light.state = "off"
-    #                 print(current_time + " " + str(light) + " counter: " + str(self.counter) + " " + str(self.number))
-    #
-    #
-    #
-    #
-    #             # print("")
-    #     #
-    #     # self.printWholeArray(current_time)
-    #
-    #     if self.counter == self.number:
-    #         light.state = "off"
-    #         self.counter = 1
-    #
-    #         print((datetime.now()).strftime("%H:%M:%S:%f") + " " + str(light) + " counter: " + str(self.counter) + " " + str(self.number))
-    #
-    #     else:
-    #         self.counter = self.counter + 1
-
-
-
-    def printWholeArray(self, current_time):
-        print(current_time + " " + str(self.traffic_whole) + " counter:" + str(self.counter))
-
-    def isOneTrafficLight(self, light):
-        if not self.isOne:
-            light.state = "on"
-            self.isOne = True
-        elif self.isOne:
-            light.state == "off"
-            self.isOne = False
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from GenerateTrafficLights import GenerateTraffic
 
 if __name__ == '__main__':
     #create x traffic lights
-    GenerateTraffic(3).runProgram()
+    GenerateTraffic(20).runProgram()
