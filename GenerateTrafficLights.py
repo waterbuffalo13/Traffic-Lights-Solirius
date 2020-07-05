ac3143f4ad603f2fe43f5434f85ffa93a0b9d92b
Patrick Fernandes <patrick.fernandes@city.ac.uk>
2020-07-05 17:07:50 +0100

    small refactors

--- GenerateTrafficLights.py
+++ GenerateTrafficLights.py
@@ -10,16 +10,11 @@ class GenerateTraffic:
     def __init__(self, number):
         self.number = number
         self.counter = 0
-        self.isOne = False
 
         self.colours_list = ["Red Light", "Green Light", "White Light"]
-        col_length = len(self.colours_list)
-
         self.colours_modified = []
         for x in range(0, self.number):
-            self.colours_modified.append(self.colours_list[x % col_length])
-
-        # self.colours_modified = list(islice(cycle(self.colours_list), number))
+            self.colours_modified.append(self.colours_list[x % len(self.colours_list)])
 
         self.traffic_whole = []
         for i, x in zip(range(number), self.colours_modified):
@@ -28,17 +23,26 @@ class GenerateTraffic:
     def runProgram(self):
         # threading.Timer(1.0, self.runProgram).start()
         while(True):
+            current_time = datetime.now().strftime("%H:%M:%S")
+
             if self.number == 1:
-                print("rip")
+                for x in self.traffic_whole:
+                    if x.state == "off":
+                        x.state = "on"
+                        print(current_time + " " + str(x))
+                    else:
+                        x.state = "off"
+                        print(current_time + " " + str(x))
+                    time.sleep(1)
+
             else:
                 index = 0
                 light = self.traffic_whole
-                current_time = datetime.now().strftime("%H:%M:%S")
+                # current_time = datetime.now().strftime("%H:%M:%S")
                 while index < len(light):
                     if light[index].position == self.counter:
                         time.sleep(1)
                         light[index].state = "on"
-
                         print(current_time + " " + str(light[index]))
 
                     #turn of behind initial variable from 0-len(9)
@@ -55,6 +59,7 @@ class GenerateTraffic:
 
                         print(current_time + " " + str(light[index]))
                     else:
+                        # print(current_time + " " + str(light[index]))
                         pass
                     index += 1
                 self.counter += 1
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from GenerateTrafficLights import GenerateTraffic
 
 if __name__ == '__main__':
-    #create x traffic lights
-    GenerateTraffic(20).runProgram()
+    while True:
+        try:
+            x = int(input("How many traffic lights do you want to generate?"))
+            GenerateTraffic(x).runProgram()
+        except ValueError:
+            print("Oops!  That was not a valid number.  Try again...")
+
+
