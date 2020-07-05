10c44ed2bfd798580b94cec92593f58272fefaa9
Patrick Fernandes <patrick.fernandes@city.ac.uk>
2020-07-05 17:15:10 +0100

    better working version

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from GenerateTrafficLights import GenerateTraffic
 
 if __name__ == '__main__':
-    while True:
-        try:
-            x = int(input("How many traffic lights do you want to generate?"))
-            GenerateTraffic(x).runProgram()
-        except ValueError:
-            print("Oops!  That was not a valid number.  Try again...")
+    try:
+        x = int(input("How many traffic lights do you want to generate? (default = 20)"))
+        GenerateTraffic(x).runProgram()
+    except ValueError:
+        print("Oops!  That was not a valid number.  Let's try with twenty")
+        x = 20
+        GenerateTraffic(x).runProgram()
 
 
